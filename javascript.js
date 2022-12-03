cbdc916320d8b005490b266f7a9f52778c1bed2a
jody lipkin <lipkinjody@gmail.com>
2022-12-02 17:24:01 -0800

    Clear function and reset

--- javascript.js
+++ javascript.js
@@ -12,7 +12,11 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
-    return num1 / num2
+    if (num2 == 0) {
+        return "Nice try"
+    } else {
+        return num1 / num2
+    }
 }
 
 function operate(num1, num2, operator){
@@ -25,7 +29,7 @@ function operate(num1, num2, operator){
     } else if (operator == "-") {
         return subtract(num1, num2)
     } else {
-        display == "OOPS"
+        return "OOPS"
     }
 }
 
@@ -34,7 +38,7 @@ function operate(num1, num2, operator){
 let display = ""
 let num1;
 let num2;
-let operand;
+let oper;
 
 //NUMBERS
 let numbers = document.querySelectorAll(".number")
@@ -49,16 +53,15 @@ numbers.forEach(number =>
 let operators = document.querySelectorAll(".operation")
 operators.forEach(operator => {
     operator.addEventListener('click', (e) => {
-        //if an operator is clicked, make what's currently displayed, num1
-            //clear the display
-            //make the operator the operand
-        if (!operand) {
+        //if an operator is clicked,
+        if (!oper) {
+            //make what's currently displayed, num1
             console.log(parseInt(display));
             num1 = parseInt(display);
-
-            operand = e.target.innerText;
+            //make the operator the operand
+            oper = e.target.innerText;
             console.log(e.target.innerText);    
-
+            //clear the display
             display = "";
             document.getElementById("display").innerText = display;
         } else {
@@ -67,10 +70,18 @@ operators.forEach(operator => {
             //display the answer 
             num2 = parseInt(display);
             console.log(num2);
-            display = operate(num1, num2, operand);
+
+            display = operate(num1, num2, oper);
+            
             console.log(display);
             document.getElementById("display").innerText = display;
-            //reset
+            //RESET
+            //set operand to the last one clicked
+            oper = e.target.innerText;
+            //set num1 to the current display
+            num1 = display;
+            //clear the display for the next number
+            display = "";
         }
     })
 })
@@ -78,9 +89,9 @@ operators.forEach(operator => {
 
 //CLEAR
 document.querySelector("#clear").addEventListener('click', function(){
-    num1 = "";
-    num2 = "";
-    operand = "";
-    display = "";
+     num1 = "";
+     num2 = "";
+     oper = "";
+     display = ""
     document.getElementById("display").innerText = display;
 })
